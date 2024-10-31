9dea8de013044108c3cac2b0c06164f671c0b15c
shohjaxon09092005 <qodiraliyevshohjaxon2005@gmail.com>
2024-10-31 15:17:43 +0500

    first commit

---
+++ src/components/MashinaAnimation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import mashina from '../image/mashina balonsiz.png'
+// import ballon from '../image/ballon.png'
+import '../style/mashinaAnimation.css'
+function MashinaAnimation() {
+    
+  return (
+    <div className='video-background' >
+  
+    </div>
+  )
+}
+
+export default MashinaAnimation
